50e294d1740f223b4c53bdb798d76f539fd7405d
Vinícius Ribeior Lopes <vi1719nicius@gmail.com>
2025-08-21 16:29:20 -0300

    feat: add HTTP routing for CRUD operations and update CORS headers

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const db = require('./conf/autenticacao.js');
-const express = require('express');
-let bodyParser = require('body-parser');
-let cors = require('cors');
-let methodOvirride = require('method-override');
+const db = require("./conf/autenticacao.js");
+const express = require("express");
+let bodyParser = require("body-parser");
+let cors = require("cors");
+let methodOvirride = require("method-override");
 const app = express();
 const port = 3000;
 
 // Permite que você use verbos HTTP
-app.use(methodOvirride('X-HTTP-Method'));
-app.use(methodOvirride('X-HTTP-Method-Override'));
-app.use(methodOvirride('X-Method-Override'));
-app.use(methodOvirride('_method'));
+app.use(methodOvirride("X-HTTP-Method"));
+app.use(methodOvirride("X-HTTP-Method-Override"));
+app.use(methodOvirride("X-Method-Override"));
+app.use(methodOvirride("_method"));
 
 app.use((req, resp, next) => {
   resp.header("Acess-Control-Allow-Origin", "*");
-  resp.header("Acess-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accep");
-  next()
+  resp.header(
+    "Acess-Control-Allow-Headers",
+    "Origin, X-Request-With, Content-Type, Accep"
+  );
+  next();
 });
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 //ROTEAMENTO RAIZ
-app.get('/', async (req, res) => {
-  const results =  await db.selectFull();
+app.get("/", async (req, res) => {
+  const results = await db.selectFull();
   console.log(results);
   res.json(results);
 });
 
 //ROTEAMENTO PARA BUSCAR PELO ID
-app.get('/clientes/:id', async (req, res) => {
+app.get("/clientes/:id", async (req, res) => {
   const id = req.params.id;
   const results = await db.selectById(id);
   console.log(results);
@@ -40,14 +40,30 @@ app.get('/clientes/:id', async (req, res) => {
 });
 
 //ROTEAMENTO PARA INSERIR
-app.post('/clientes', async (req, res) => {
-  const { nome, email, idade } = req.body;
-  const results = await db.insertCliente(nome, email, idade);
+app.post("/clientes/", async (req, res) => {
+  const { nome, idade, uf } = req.body;
+  const results = await db.insertCliente(nome, idade, uf);
+  console.log(results);
+  res.json(results);
+});
+
+//ROTEAMENTO PARA ATUALIZAR
+app.put("/clientes/:id", async (req, res) => {
+  const id = req.params.id;
+  const { nome, idade, uf } = req.body;
+  const results = await db.updateCliente(id, nome, idade, uf);
+  console.log(results);
+  res.json(results);
+});
+
+//ROTEAMENTO PARA DELETAR
+app.delete("/clientes/:id", async (req, res) => {
+  const id = req.params.id;
+  const results = await db.deleteCliente(id);
   console.log(results);
   res.json(results);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
